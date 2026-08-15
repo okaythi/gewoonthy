cadcc39a099e3e8bebfe3be00ee8152d02c67728
okaythi <213379314+okaythi@users.noreply.github.com>
2026-08-15 03:10:55 +0100

    fix: race condition between flask keep_alive thread and register_routes

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -9,9 +9,9 @@ from features.reactions import ReactionManager
 load_dotenv(os.path.join(os.path.dirname(__file__), "..", ".env"))
 
 if __name__ == '__main__':
-    keep_alive()
     client = MyClient()
     register_routes(client)
+    keep_alive()
     reaction_manager = ReactionManager(client)
     register_default_commands(client.command_engine, reaction_manager)
     client.reaction_manager = reaction_manager
